f222b78431595630624bad77915b2711c0264cd4
IsrailGulzar <68149527+israilgulzar@users.noreply.github.com>
2021-06-03 12:48:24 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
